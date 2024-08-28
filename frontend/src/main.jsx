fb5d009c3d8d49d19977aae8550781c0551ba629
Weber Souza <weberv.souza@gmail.com>
2024-08-28 08:01:24 -0300

    Upload struct code App, Main and Index

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import React from 'react'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
+)

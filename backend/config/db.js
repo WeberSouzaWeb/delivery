6a18db14ce3fa6cba5784a880fb07d16801d1178
Weber Souza <weberv.souza@gmail.com>
2024-08-29 14:16:03 -0300

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,5 @@
+import mongoose from "mongoose";
 
+export const connectDB = async () =>{
+    await mongoose.connect('mongodb+srv://websette:11237594@cluster-food.uhtju.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+}
